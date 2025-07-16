afddea68fac1732411dc177c495020daa08e5dc2
Ahmed <ahmed.osaid.pro@gmail.com>
2025-07-16 08:37:14 -0500

    Initial Commit

---
+++ Team Wellness CompanyTests/Team_Wellness_CompanyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Team_Wellness_CompanyTests.swift
+//  Team Wellness CompanyTests
+//
+//  Created by Ahmed Said on 7/16/25.
+//
+
+import Testing
+@testable import Team_Wellness_Company
+
+struct Team_Wellness_CompanyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
